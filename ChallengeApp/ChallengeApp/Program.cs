b6db743426374d33722ced3d7859f26fd09ea267
Maciej Jasiński <amaciejjasinski@gmail.com>
2023-07-18 09:24:11 +0200

    Zadanie domowa dzień 4.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zadanie domowe dzień 4.
+
+string name1 = "Monika";
+string name2 = "Maciej";
+int Age1 = 28;
+int Age2 = 16;
+string result1 = name1 + Age1;
+string result2 = name2 + Age2;
+
+    Console.WriteLine(result1);
+
+if (name1 == "Monika");
+
+{
+    Console.WriteLine("Jesteś kobietą");
+}
+
+if (Age1 < 30);
+{
+
+    Console.WriteLine("Poniżej 30 lat");
+}
+    Console.WriteLine(result2);
+
+if (Age2 < 18);
+
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+    
+}
